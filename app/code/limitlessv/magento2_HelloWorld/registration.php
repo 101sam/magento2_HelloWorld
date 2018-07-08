e48810e1d06c7e30a2669bf20855efca855696c8
Shmuel Maruani <13782917++limitlessv@users.noreply.github.com>
2018-07-08 15:05:09 -0700

    Tests

--- app/code/limitlessv/magento2_HelloWorld/registration.php
+++ app/code/limitlessv/magento2_HelloWorld/registration.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: shmuelm
- * Date: 7/8/18
- * Time: 2:23 PM
- */
-<?php
 \Magento\Framework\Component\ComponentRegistrar::register(
         \Magento\Framework\Component\ComponentRegistrar::MODULE,
         'magento2_HelloWorld',
